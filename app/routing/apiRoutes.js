4417f2e09f284453f8a44111d9061742677cb325
RomeoKilo125 <romeokilo125@gmail.com>
2018-06-14 16:43:53 -0400

    first draft of match making logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,31 @@
 let friends = require('../data/friends')
 
+function findMatch(sub) {
+  let result = "";
+  let score = 50;
+  for (friend in friends) {
+    let test = calculateScore(sub.scores, friend.scores);
+    if (test < score) {
+      score = test;
+      result = friend;
+    }
+  }
+  return result;
+}
+
 module.exports = function(app) {
 
   app.get('/api/friends', (req, res) => {
     res.json(friends);
   });
 
-  app.post('api/friends', (req, res) => {
+  app.post('/api/friends', (req, res) => {
+    let newFriend = req.body;
+    
+    let match = findMatch(newFriend)
+    res.json(match);
+
+    friends.push(newFriend);
 
   });
 

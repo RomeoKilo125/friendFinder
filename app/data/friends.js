402b031b2b75af676a3d1eb18d5bad3033b6bd68
RomeoKilo125 <romeokilo125@gmail.com>
2018-06-14 16:55:52 -0400

    answered questions for pre-existing pool

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,98 +1,116 @@
-let friends = [{
-  "name": "Iroh",
-  "photo": "https://vignette.wikia.nocookie.net/avatar/images/c/c1/Iroh_smiling.png/revision/latest?cb=20130626131914",
-  "scores": [
-    5,
-    1,
-    4,
-    4,
-    5,
-    1,
-    2,
-    5,
-    4,
-    1
-  ]
-},
-{
-  "name": "Sokka",
-  "photo": "https://vignette.wikia.nocookie.net/avatar/images/7/76/Grinning_Sokka.png/revision/latest?cb=20140122221732",
-  "scores": [
-    5,
-    1,
-    4,
-    4,
-    5,
-    1,
-    2,
-    5,
-    4,
-    1
-  ]
-},
-{
-  "name": "Aang",
-  "photo": "https://vignette.wikia.nocookie.net/avatar/images/a/ae/Aang_at_Jasmine_Dragon.png/revision/latest?cb=20130612174003",
-  "scores": [
-    5,
-    1,
-    4,
-    4,
-    5,
-    1,
-    2,
-    5,
-    4,
-    1
-  ]
-},
-{
-  "name": "Katara",
-  "photo": "https://vignette.wikia.nocookie.net/avatar/images/7/7a/Katara_smiles_at_coronation.png/revision/latest?cb=20150104171449",
-  "scores": [
-    5,
-    1,
-    4,
-    4,
-    5,
-    1,
-    2,
-    5,
-    4,
-    1
-  ]
-},
-{
-  "name": "Zuko",
-  "photo": "https://vignette.wikia.nocookie.net/avatar/images/4/4b/Zuko.png/revision/latest?cb=20140628142636",
-  "scores": [
-    5,
-    1,
-    4,
-    4,
-    5,
-    1,
-    2,
-    5,
-    4,
-    1
-  ]
-},
-{
-  "name": "Toph",
-  "photo": "https://vignette.wikia.nocookie.net/avatar/images/4/46/Toph_Beifong.png/revision/latest?cb=20131230122047",
-  "scores": [
-    5,
-    1,
-    4,
-    4,
-    5,
-    1,
-    2,
-    5,
-    4,
-    1
-  ]
-}]
+let friends = [
+  {
+    "name": "Iroh",
+    "photo": "https://vignette.wikia.nocookie.net/avatar/images/c/c1/Iroh_smiling.png/revision/latest?cb=20130626131914",
+    "scores": [
+      4,
+      4,
+      4,
+      4,
+      4,
+      1,
+      3,
+      3,
+      1,
+      5
+    ]
+  },
+  {
+    "name": "Sokka",
+    "photo": "https://vignette.wikia.nocookie.net/avatar/images/7/76/Grinning_Sokka.png/revision/latest?cb=20140122221732",
+    "scores": [
+      5,
+      4,
+      3,
+      2,
+      2,
+      5,
+      4,
+      4,
+      2,
+      3
+    ]
+  },
+  {
+    "name": "Aang",
+    "photo": "https://vignette.wikia.nocookie.net/avatar/images/a/ae/Aang_at_Jasmine_Dragon.png/revision/latest?cb=20130612174003",
+    "scores": [
+      5,
+      5,
+      5,
+      5,
+      5,
+      3,
+      3,
+      4,
+      1,
+      5
+    ]
+  },
+  {
+    "name": "Katara",
+    "photo": "https://vignette.wikia.nocookie.net/avatar/images/7/7a/Katara_smiles_at_coronation.png/revision/latest?cb=20150104171449",
+    "scores": [
+      5,
+      2,
+      2,
+      4,
+      4,
+      2,
+      2,
+      1,
+      2,
+      2
+    ]
+  },
+  {
+    "name": "Zuko",
+    "photo": "https://vignette.wikia.nocookie.net/avatar/images/4/4b/Zuko.png/revision/latest?cb=20140628142636",
+    "scores": [
+      1,
+      5,
+      1,
+      1,
+      3,
+      5,
+      4,
+      5,
+      2,
+      4
+    ]
+  },
+  {
+    "name": "Toph",
+    "photo": "https://vignette.wikia.nocookie.net/avatar/images/4/46/Toph_Beifong.png/revision/latest?cb=20131230122047",
+    "scores": [
+      1,
+      2,
+      5,
+      2,
+      3,
+      2,
+      4,
+      5,
+      5,
+      3
+    ]
+  },
+  {
+    "name": "Appa",
+    "photo": "https://vignette.wikia.nocookie.net/avatar/images/6/65/Appa_flying.png/revision/latest?cb=20140517110636",
+    "scores": [
+      4,
+      2,
+      3,
+      5,
+      5,
+      3,
+      1,
+      4,
+      4,
+      2
+    ]
+  }
+]
 
 module.exports = friends;

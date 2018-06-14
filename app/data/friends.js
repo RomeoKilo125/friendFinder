73cd64e59a533495552d9040ee489fc26a8f8184
RomeoKilo125 <romeokilo125@gmail.com>
2018-06-14 11:33:29 -0400

    added multiple friends to the pool

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,86 @@
 let friends = [{
-  "name": "Ahmed",
-  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "name": "Iroh",
+  "photo": "https://vignette.wikia.nocookie.net/avatar/images/c/c1/Iroh_smiling.png/revision/latest?cb=20130626131914",
+  "scores": [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+  ]
+},
+{
+  "name": "Sokka",
+  "photo": "https://vignette.wikia.nocookie.net/avatar/images/7/76/Grinning_Sokka.png/revision/latest?cb=20140122221732",
+  "scores": [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+  ]
+},
+{
+  "name": "Aang",
+  "photo": "https://vignette.wikia.nocookie.net/avatar/images/a/ae/Aang_at_Jasmine_Dragon.png/revision/latest?cb=20130612174003",
+  "scores": [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+  ]
+},
+{
+  "name": "Katara",
+  "photo": "https://vignette.wikia.nocookie.net/avatar/images/7/7a/Katara_smiles_at_coronation.png/revision/latest?cb=20150104171449",
+  "scores": [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+  ]
+},
+{
+  "name": "Zuko",
+  "photo": "https://vignette.wikia.nocookie.net/avatar/images/4/4b/Zuko.png/revision/latest?cb=20140628142636",
+  "scores": [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+  ]
+},
+{
+  "name": "Toph",
+  "photo": "https://vignette.wikia.nocookie.net/avatar/images/4/46/Toph_Beifong.png/revision/latest?cb=20131230122047",
   "scores": [
     5,
     1,

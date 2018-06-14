05c52b764fc42c72fc385c6c8d4527d0aa64753c
RomeoKilo125 <romeokilo125@gmail.com>
2018-06-14 11:33:58 -0400

    cleaned up code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,12 +2,12 @@ let friends = require('./app/data/friends')
 
 module.exports = function(app) {
 
-app.get('/api/friends', (req, res) => {
-  res.json(friends);
-});
+  app.get('/api/friends', (req, res) => {
+    res.json(friends);
+  });
 
-app.post('api/friends', (req, res) => {
-  
-});
+  app.post('api/friends', (req, res) => {
+
+  });
 
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,13 @@
 let path = require('path');
 
-  app.get('/survey', (req, res) => {
-    res.sendFile(path.join(__dirname, './app/public/survey.html'))
-  });
 
   module.exports = function(app) {
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, './app/public/home.html'))
+    });
+
+    app.get('/survey', (req, res) => {
+      res.sendFile(path.join(__dirname, './app/public/survey.html'))
     });
 
 };
